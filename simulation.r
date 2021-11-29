9bcb830928f82d5caa1b1a266dc1f339eaf12542
Arthur Joschka von der Heyden <92376936+whatisarthurdoing@users.noreply.github.com>
2021-11-29 22:17:56 +0100

    added simulation

---
+++ simulation.r
@@ -0,0 +1,9 @@
+#(1) alter, als Normalverteilung mit Erwartungswert 25 und Standardabweichung 2
+
+#(2) studienfach, zufaellig gezogen fuer alle Personen aus Auswahl
+#    "Statistik", "Data Science", "Mathe", "Informatik"
+#    Gleiche Wahrscheinlichkeit: DS und S
+#    Geringere Wahrscheinlichkeit: I
+#    Geringste Wahrscheinlichkeit: M
+
+#(3) interesse_an_Mathematik, asdf
