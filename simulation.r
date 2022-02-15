907910efd70b31c1cee30076ad909a86edcc9106
DaLxx <94531865+DaLxx@users.noreply.github.com>
2022-02-15 06:12:56 +0100

    Update simulation.r
    Teil (4) Intresse an Programmieren

--- simulation.r
+++ simulation.r
@@ -28,3 +28,25 @@ simulate_interest_math <- function( studienfach ){
 }
 interesse_an_mathe <- simulate_interest_math(studienfach)
 interesse_an_mathe
+
+# (4) Interesse an Programmieren:
+# Interesse an Programmieren: Antworten 1-6 beliebig = Mathestudent*in, 
+# Antworten 3-7 mit Wahscheinlichkeiten = Statistik- oder Data Sciencestudent*in 
+# und 7 = Informatikstudent*in
+simulate_interest_prog <- function( studienfach ){
+  iap <- c()
+  for (i in studienfach){
+    if(i == "Mathematik"){
+      iap <- append(iap, sample(1:6), replace = TRUE)
+    }
+    if(i == "Statistik" | i == "Data Science"){
+      iap <- append(iap, sample(3:7, replace = TRUE, prob = c(0.1, 0.2, 0.25, 0.25, 0.2)))
+    }
+    if(i == "Informatik"){
+      iap <- append(iap, 7)
+    }
+  }
+  return(iap)
+}
+interesse_an_prog <- simulate_interest_prog(studienfach)
+interesse_an_prog
