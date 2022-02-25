72bae7a63482d7e1c461a506a195002b72831622
DaLxx <94531865+DaLxx@users.noreply.github.com>
2022-02-25 07:43:54 +0100

    Create Funktionen-Skript1.R
    Funktionen zur Analyse des Datensatzes

---
+++ Funktionen-Skript1.R
@@ -0,0 +1,16 @@
+# Funktionen-Skript 1
+# x = Datensatz, der analysiert werden soll
+
+
+# (a) Eine Funktion, die verschiedene geeignete deskriptive Statistiken für metrische Variablen 
+# berechnet und ausgibt
+
+
+# (b) Eine Funktion, die verschiedene geeignete deskriptive Statistiken für kategoriale Variablen 
+# berechnet und ausgibt
+
+
+# (c) Eine Funktion, die geeignete deskriptive bivariate Statistiken für den Zusammenhang zwischen 
+# zwei kategorialen Variablen berechnet ausgibt
+
+apply(x, 2, table)
