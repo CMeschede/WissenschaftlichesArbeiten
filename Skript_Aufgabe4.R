0227b13b3d24cfe39b7ed9fe79fa204c6b816520
valerieweismann <94704207+valerieweismann@users.noreply.github.com>
2022-02-28 18:04:27 +0100

    Visualisierungen mithilfe der Funktionen aus (f)
    Wende die Funktionen aus Teilaufgabe (f) auf die Daten an, um Mosaik- und Barplots zu erstellen

--- Skript_Aufgabe4.R
+++ Skript_Aufgabe4.R
@@ -16,3 +16,12 @@ daten$mathe_LK <- as.factor(daten$mathe_LK)
 # (e)
 
 # (f)
+
+TeilF1(daten$interesse_an_mathe,  daten$studienfach, daten$mathe_LK,
+       main = "Mosaikplot: Interesse an Mathe, Studienfach, Mathe LK",
+       xlab = "Interesse an Mathe", ylab = "Studienfach",
+       col = c("steelblue1", "springgreen3"))
+
+TeilF2(daten[2:6], main = c("Alter", "Studienfach", "Interesse an Mathe",
+                            "Interesse an Programmieren", "Mathe LK"))
+
